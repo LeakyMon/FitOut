593617f49619bc94d44c010d8e23baa464661a5a
LeakyMon <hmonforte17@gmail.com>
2024-02-20 17:44:20 -0500

    Npx react app created

---
+++ fitout/src/Homepage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Homepage(){
+    return (
+        <div className='homepage'>
+            <div className="homepage_nav">
+
+            </div>
+            <div className="homepage_timeline">
+                
+            </div>
+        </div>
+    )
+
+
+}
