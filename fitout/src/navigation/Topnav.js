85b0f97b0853aaab7e8300dbae7ec647f7da047b
LeakyMon <hmonforte17@gmail.com>
2024-02-20 20:12:19 -0500

    profile pic right top

--- fitout/src/navigation/Topnav.js
+++ fitout/src/navigation/Topnav.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import "./Topnav.css"
+import {Avatar} from "@mui/material"
 
 function Topnav() {
   return (
     <div className="abovePost">
-        <p>New social media FitOut! Connect with your friends, post your outfits and more!</p>
+        <p className="line">New social media FitOut! Connect with your friends, post your outfits and more!</p>
+        <Avatar onClick="" className="RightProfile" >
+            <img className="logo"src="/FitOutLogo.webp"></img>
+        </Avatar>
     </div>
   )
 }
