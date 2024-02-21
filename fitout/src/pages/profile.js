3b74635ec58dfc66805bf6989f77d778b139fa6a
LeakyMon <hmonforte17@gmail.com>
2024-02-21 01:18:09 -0500

    Basic profile page

--- fitout/src/pages/profile.js
+++ fitout/src/pages/profile.js
@@ -10,22 +10,24 @@ function Profile() {
     return (
         <main>
             {user ? (
-                <div className="profileContainer">
+                <><div className="profileContainer">
                     <div className="ProfileHeader">
                         <img className="pfp" src={user.profilePicture || ""} alt="Profile" />
                         <div className="profileInfoAndActions">
                             <div className="profileInfo">
-                                <div><strong>{user.numPosts}</strong> Posts&nbsp;</div>
-                                <div><strong>{user.followerCount}</strong> Followers&nbsp;</div>
+                                <div><strong>{user.numPosts}</strong> Posts&nbsp;&nbsp;&nbsp;</div>
+                                <div><strong>{user.followerCount}</strong> Followers&nbsp;&nbsp;&nbsp;</div>
                                 <div><strong>{user.followingCount}</strong> Following</div>
                             </div>
                             <button className="followButton">Follow</button>
+                            
                         </div>
                     </div>
-                    <div className="ProfileValues">
-                        <h3>{user.username}</h3>
-                    </div>
+
                 </div>
+                <div className="ProfileValues">
+                    <h3>{user.username}</h3>
+                </div></>
             ) : (
                 <p>Loading user data...</p>
             )}
